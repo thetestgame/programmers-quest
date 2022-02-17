e655beb3514baffe8c56101eab6aa36ab44ec48e
thetestgame <nxtlabs13@gmail.com>
2022-02-17 17:02:28 -0600

    Removed old copyright headers

--- quest/world/entity.py
+++ quest/world/entity.py
@@ -1,23 +1,3 @@
-"""
- * Copyright (C) Nxt Games 2021
- * All Rights Reserved
- *
- * Written by Jordan Maxwell <jordan.maxwell@nxt-games.com>, January 7th, 2021
- *
- * NXT GAMES CONFIDENTAL
- * _______________________
- *
- * NOTICE:  All information contained herein is, and remains
- * the property of Nxt Games and its suppliers,
- * if any. The intellectual and technical concepts contained
- * herein are proprietary to Nxt Games
- * and its suppliers and may be covered by U.S. and Foreign Patents,
- * patents in process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Nxt Games.
-"""
-
 from quest.engine import core, prc, showbase
 from quest.engine import runtime, vfs
 from quest.framework import runnable
