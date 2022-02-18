9f48df189ff4d607499e6cf94dbc20616b1f0a20
thetestgame <nxtlabs13@gmail.com>
2022-02-17 23:44:23 -0600

    Added proper Docker/production startup scripts

--- quest/ai/service.py
+++ quest/ai/service.py
@@ -1,16 +1,16 @@
 import sys
-from quest.client import application
+from quest.ai import application
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
 def main() -> int:
     """
-    Main entry point into the Questlike MMO client application
+    Main entry point into the Programmer's Quest MMO UberDOG server application
     """
 
-    return application.main(development=True)
+    return application.main(development=False)
 
-# Main entry point into the Questlike MMO client application
+# Main entry point into the Programmer's Quest MMO UberDOG server application
 if __name__ == '__main__':
     sys.exit(main())
 
