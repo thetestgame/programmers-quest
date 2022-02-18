9c1ed9039c5f307c7f86c92ea88dc32f79969f4a
thetestgame <nxtlabs13@gmail.com>
2022-02-18 12:21:16 -0600

    Added skeleton of shard objects

--- quest/distributed/shard.py
+++ quest/distributed/shard.py
@@ -1,23 +1,30 @@
-from quest import client
 from quest.distributed import objects
 from quest.engine import runtime
-from quest.client import network as client_network
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
-class DistributedShardServer(objects.QuestDistributedObject):
+class DistributedShardServerBase(object):
     """
+    Base shared class for all shard server objects
     """
 
-    neverDisable = 1 # Legacy magic value from Pandas original DO implementation
-
-    def __init__(self, cr: object):
-        super().__init__(cr)
-
+    def __init__(self):
         self.name = 'Undefined'
         self.available = 0
-        self.playerCount = 0
-        self.newPlayerCount = 0
+
+    def _get_repository() -> object:
+        """
+        Returns our repository instance
+        """
+
+        repository = None
+        if runtime.has_cr():
+            repository = runtime.cr
+        elif runtime.has_air():
+            repository = runtime.air
+
+        assert repository != None, 'No repository found on runtime'
+        return repository
 
     def announceGenerate(self) -> None:
         """
@@ -25,20 +32,21 @@ class DistributedShardServer(objects.QuestDistributedObject):
 
         super().announceGenerate()
 
-        cr = client_network.QuestClientNetworkManager.get_singleton()
-        cr.active_shard_map[self.doId] = self
+        repository = self._get_repository()
+        repository.active_shard_map[self.doId] = self
         self.send_update_event()
 
     def delete(self) -> None:
         """
         """
 
-        cr = client_network.QuestClientNetworkManager.get_singleton()
-        if cr.active_shard is self:
-            cr.active_shard = None
+        if runtime.has_cr():
+            if runtime.cr.active_shard is self:
+                runtime.cr.active_shard = None
         
-        if self.doId in cr.active_shard_map:
-            del cr.active_shard_map[self.doId]
+        repository = self._get_repository()
+        if self.doId in repository.active_shard_map:
+            del repository.active_shard_map[self.doId]
 
         super().delete()
         self.send_update_event()
@@ -63,24 +71,6 @@ class DistributedShardServer(objects.QuestDistributedObject):
         self.name = name
         self.send_update_event()
 
-#----------------------------------------------------------------------------------------------------------------------------------------------------------------------#
-
-class DistributedShardServerAI(objects.DistributedObjectAI):
-    """
-    """
-
-    def __init__(self, air: object, name: str = "Undefined"):
-        super().__init__(air)
-        self.air = air
-        self.name = name
-        self.available = 0
-        
-    def delete(self):
-        self.ignore_all()
-        self.b_set_available(0)
-        
-        super().delete(self)        
-    
     def get_available(self) -> bool:
         """
         """
@@ -92,12 +82,39 @@ class DistributedShardServerAI(objects.DistributedObjectAI):
         """
 
         return self.name
-    
-    def set_available(self, available: bool) -> None:
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+class DistributedShardServer(objects.QuestDistributedObject, DistributedShardServerBase):
+    """
+    Client side implementation of the distributed shard server object used to identify AI server instances
+    """
+
+    neverDisable = 1 # Legacy magic value from Pandas original DO implementation
+
+    def __init__(self, cr: object):
+        super().__init__(cr)
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+class DistributedShardServerAI(objects.DistributedObjectAI, DistributedShardServerBase):
+    """
+    AI side implementation of the distributed shard server object used to identify AI server instances
+    """
+
+    def __init__(self, air: object, name: str = "Undefined"):
+        super().__init__(air)
+        self.air = air
+        self.air.active_shard = self
+        
+    def delete(self) -> None:
         """
         """
 
-        self.available = available
+        self.ignore_all()
+        self.b_set_available(0)
+        
+        super().delete(self)
     
     def d_set_available(self, available: bool) -> None:
         """
@@ -111,12 +128,6 @@ class DistributedShardServerAI(objects.DistributedObjectAI):
 
         self.set_available(available)
         self.d_set_available(available)
-            
-    def set_name(self, name: str) -> None:
-        """
-        """
-
-        self.name = name
                
     def d_set_name(self, name: str) -> None:
         """
@@ -133,8 +144,9 @@ class DistributedShardServerAI(objects.DistributedObjectAI):
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
-class DistributedShardServerUD(objects.QuestDistributedObjectUD):
+class DistributedShardServerUD(objects.QuestDistributedObjectUD, DistributedShardServerBase):
     """
+    UberDOG side implementation of the distributed shard server object used to identify AI server instances
     """
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------#
